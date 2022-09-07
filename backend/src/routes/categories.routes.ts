bb11651cfea0804bdd1556283852be2b790e10e3
Gregory Engraf <gregory0409@gmail.com>
2022-09-06 21:56:53 -0300

    :sparkles: Implement typescrip, eslint, pretier and other initial configurations

---
+++ backend/src/routes/categories.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import multer from "multer";
+
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+
+const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
+
+categoriesRoutes.post("/", (req, res) => {
+  return createCategoryController.handle(req, res);
+});
+
+categoriesRoutes.get("/", (req, res) => {
+  return listCategoriesController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+  console.log(file);
+  return res.send();
+});
+
+export { categoriesRoutes };
