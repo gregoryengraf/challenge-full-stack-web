054659377fad70eece685ebc9a4d1c77a2f9150f
Gregory Engraf <gregory0409@gmail.com>
2022-09-13 15:24:55 -0300

    :sparkles: Implement screenshots

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,7 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase text-center">
-        <span>+ A </span>
-        <span class="font-weight-light">Educação</span>
+        <img class="ma-2" src="https://maisaedu.com.br/hubfs/site-grupo-a/logo-mais-a-educacao.svg" alt="">
       </v-toolbar-title>
     </v-toolbar>
     <v-main>
