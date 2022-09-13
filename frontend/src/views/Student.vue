4b3639c67b65efe987296e8995474f03714d0316
Gregory Engraf <gregory0409@gmail.com>
2022-09-13 10:50:45 -0300

    :construction: WIP student components

--- frontend/src/views/Student.vue
+++ frontend/src/views/Student.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <v-row>
+      <v-col cols="10">
+        <v-text-field>
+          <v-icon icon="mdi-magnify"></v-icon>
+        </v-text-field>
+      </v-col>
+      <v-col cols="2">
+        <v-btn class="ma-2" color="green" icon="mdi-plus"></v-btn>
+      </v-col>
+    </v-row>
     <v-table>
       <thead>
         <tr>
@@ -16,7 +26,12 @@
           <td>jose@teste.com</td>
           <td>23423423</td>
           <td>SDF34</td>
-          <td></td>
+          <td>
+            <div>
+              <v-btn class="ma-2" color="blue" icon="mdi-pencil"></v-btn>
+              <v-btn class="ma-2" color="red" icon="mdi-delete"></v-btn>
+            </div>
+          </td>
         </tr>
       </tbody>
     </v-table>
